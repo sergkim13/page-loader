3fa7074e2cafd2e8c951f324afbe2a4fd27ecfca
Sergey Kim <sergkim7@gmail.com>
2022-11-21 22:48:01 +0300

    upd parser.py

--- page_loader/args_parser.py
+++ page_loader/args_parser.py
@@ -5,7 +5,7 @@ import os
 def get_args():
     page_loader = argparse.ArgumentParser(
         description='Downloads page and saves content to file in set dir.')
-    page_loader.add_argument('url')
+    page_loader.add_argument('URL')
     page_loader.add_argument('-o', '--output', help='set directory',
                              default=os.getcwd())
     args = page_loader.parse_args()
