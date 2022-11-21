7f5a951f41399dbb985e353e43b2af9e76ab0562
Sergey Kim <sergkim7@gmail.com>
2022-11-21 21:53:58 +0300

    upd test

---
+++ tests/test_download.py
@@ -0,0 +1,19 @@
+from page_loader import download
+import requests_mock
+import tempfile
+
+
+expected_html_path = 'tests/fixtures/expected_page.html'
+mock_page_path = 'tests/fixtures/mock_page_content.txt'
+mock_text = open(mock_page_path).read()
+url = 'https://ru.hexlet.io/courses'
+
+
+def test_download():
+    with requests_mock.Mocker() as m:
+        m.get(url, text=mock_text)
+        with tempfile.TemporaryDirectory() as temp_dir:
+            result_page_path = download(url, dir=temp_dir)
+            result_page = open(result_page_path).read()
+            expected_html_page = open(expected_html_path).read()
+            assert result_page == expected_html_page
