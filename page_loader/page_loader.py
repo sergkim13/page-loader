db0c98d6356fc66a95afbe457314acb1831890d6
Ким Сергей Эдуардович <sergkim7@gmail.com>
2022-11-21 14:37:49 +0300

    upd page_loader.py

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,2 +1,11 @@
-def download():
+import os, re
+
+def download(url, dir=os.getcwd()):  
     pass
+
+
+def get_file_name(url):
+    _, address = url.split('//')
+    file_ext = '.html'
+    name = re.sub(r'\W', '-', address) + file_ext
+    return name
