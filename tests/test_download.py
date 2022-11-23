f4ee73d80e0f04876861f938849dfaf27b6979e8
Sergey Kim <sergkim7@gmail.com>
2022-11-23 21:40:10 +0300

    upd tests

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,9 +1,11 @@
 from page_loader import download
 import requests_mock
 import tempfile
+import os
 
 
 expected_html_path = 'tests/fixtures/expected_page.html'
+expected_html_files_path = 'ru-hexlet-io-courses_files'
 mock_page_path = 'tests/fixtures/mock_page_content.txt'
 mock_text = open(mock_page_path).read()
 url = 'https://ru.hexlet.io/courses'
@@ -14,9 +16,11 @@ def test_download():
         m.get(url, text=mock_text)
         with tempfile.TemporaryDirectory() as temp_dir:
             result_page_path = download(url, temp_dir)
+            result_html_files_path = os.path.join(temp_dir, expected_html_files_path)
             result_page = open(result_page_path).read()
             expected_page = open(expected_html_path).read()
             assert result_page == expected_page
+            assert not len(os.listdir(result_html_files_path)) == 0
 
 
 def test_download_to_not_exist_dir():
