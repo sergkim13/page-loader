af9fd532f6dd085cd82c324fdc4d2929d94c8adc
Ким Сергей Эдуардович <sergkim7@gmail.com>
2022-11-24 10:08:12 +0300

    upd tests

--- tests/test_download.py
+++ tests/test_download.py
@@ -4,23 +4,34 @@ import tempfile
 import os
 
 
-expected_html_path = 'tests/fixtures/expected_page.html'
-expected_html_files_path = 'ru-hexlet-io-courses_files'
-mock_page_path = 'tests/fixtures/mock_page_content.txt'
-mock_text = open(mock_page_path).read()
+# Тестовые данные
 url = 'https://ru.hexlet.io/courses'
+
+path_to_expected_html_page = 'tests/fixtures/expected_page.html'
+expected_page = open(path_to_expected_html_page).read()
+
+expected_html_files_dir = 'ru-hexlet-io-courses_files'
+
+path_to_mock_page = 'tests/fixtures/mock_page_content.txt'
+mock_text = open(path_to_mock_page).read()
+
+path_to_expected_images_names = 'tests/fixtures/expected_images_names.txt'
+expected_images_names = open('path_to_expected_images_names')
+images_name_list = expected_images_names.read().split(',\n')
 
 
 def test_download():
+
     with requests_mock.Mocker() as m:
-        m.get(url, text=mock_text)
+        m.get(url, mock_text)
         with tempfile.TemporaryDirectory() as temp_dir:
             result_page_path = download(url, temp_dir)
-            result_html_files_path = os.path.join(temp_dir, expected_html_files_path)
+            result_html_files_path = os.path.join(
+                temp_dir, expected_html_files_dir)
             result_page = open(result_page_path).read()
-            expected_page = open(expected_html_path).read()
+            
             assert result_page == expected_page
-            assert not len(os.listdir(result_html_files_path)) == 0
+            assert os.listdir(result_html_files_path) == images_name_list
 
 
 def test_download_to_not_exist_dir():
