2418fe8d92af48367036c149ec13ba300e2f250b
Sergey Kim <sergkim7@gmail.com>
2022-11-24 19:24:11 +0300

    upd test_download.py

--- tests/test_generate_file_name.py
+++ tests/test_generate_file_name.py
@@ -1,4 +1,4 @@
-from page_loader.loader import generate_file_name
+from page_loader.loader import generate_page_file_name
 
 
 url = 'https://ru.hexlet.io/courses/index.html'
@@ -6,5 +6,5 @@ expected_file_name = 'ru-hexlet-io-courses-index.html'
 
 
 def test_generate_file_name():
-    result = generate_file_name(url)
+    result = generate_page_file_name(url)
     assert result == expected_file_name
