a3d8e038c52edadd88358388cce71f99b6844c33
Sergey Kim <sergkim7@gmail.com>
2022-11-24 19:24:21 +0300

    upd test_download.py

--- tests/test_download.py
+++ tests/test_download.py
@@ -16,14 +16,14 @@ path_to_mock_page = 'tests/fixtures/mock_page_content.txt'
 mock_text = open(path_to_mock_page).read()
 
 path_to_expected_images_names = 'tests/fixtures/expected_images_names.txt'
-expected_images_names = open('path_to_expected_images_names')
+expected_images_names = open(path_to_expected_images_names)
 images_name_list = expected_images_names.read().split(',\n')
 
 
 def test_download():
 
     with requests_mock.Mocker() as m:
-        m.get(url, mock_text)
+        m.get(url, text=mock_text)
         with tempfile.TemporaryDirectory() as temp_dir:
             result_page_path = download(url, temp_dir)
             result_html_files_path = os.path.join(
