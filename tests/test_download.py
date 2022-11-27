985720f23036bb16f5a9fc85472960c207c52aa9
Sergey Kim <sergkim7@gmail.com>
2022-11-27 19:39:28 +0300

    upd test of download

--- tests/test_download.py
+++ tests/test_download.py
@@ -2,14 +2,15 @@ from page_loader import download
 from page_loader.loader import download_image
 import requests_mock
 import tempfile
+import os
 
 
 # Тестовые данные
 url = 'https://ru.hexlet.io/courses'
-img1 = 'https://cdn2.hexlet.io/assets/logo_ru-495f05850e0095ea722a2b583565d492719579c02b0ce61d924e4f895fabf781.svg'  # noqa: E501
-img2 = 'https://cdn2.hexlet.io/assets/team/you-bc72575a0e6eb39de3e28e54a8df1138beaa57cd5300061ecb5c202773131f9e.jpg'  # noqa: E501
-img3 = 'https://cdn2.hexlet.io/assets/at_a_laptop-8c6e59267f91a6bf13bae0e5c0f7e1f36accc440b8d760bca08ab244e2b8bdbf.png'  # noqa: E501
-img4 = 'https://cdn2.hexlet.io/assets/flag-en-f0b48c6562bb27879fbd685ece0133271ea043384dd9793843c246f862ac7cc1.svg'  # noqa: E501
+img1 = 'https://ru.hexlet.io/assets/logo_ru-495f05850e0095ea722a2b583565d492719579c02b0ce61d924e4f895fabf781.svg'  # noqa: E501
+img2 = 'https://ru.hexlet.io/assets/team/you-bc72575a0e6eb39de3e28e54a8df1138beaa57cd5300061ecb5c202773131f9e.jpg'  # noqa: E501
+img3 = 'https://ru.hexlet.io/assets/at_a_laptop-8c6e59267f91a6bf13bae0e5c0f7e1f36accc440b8d760bca08ab244e2b8bdbf.png'  # noqa: E501
+img4 = 'https://en.hexlet.io/assets/flag-en-f0b48c6562bb27879fbd685ece0133271ea043384dd9793843c246f862ac7cc1.svg'  # noqa: E501
 img5 = 'https://cdn2.hexlet.io/assets/flag-ru-593864ce87ae202b2c2e9393b2a6cf9384ac9cbb1c70632f4c6eeca34341483e.svg'  # noqa: E501
 
 
@@ -49,22 +50,13 @@ def test_download():
         m.get(img5, content=mock_img5)
         with tempfile.TemporaryDirectory() as temp_dir:
             result_page_path = download(url, temp_dir)
-            # result_html_files_path = os.path.join(
-            #     temp_dir, expected_html_files_dir)
+            result_html_files_path = os.path.join(
+                temp_dir, expected_html_files_dir)
             result_page = open(result_page_path).read()
             assert result_page == expected_page
-            # assert os.listdir(result_html_files_path) == images_name_list
+            assert os.listdir(result_html_files_path) == images_name_list
 
 
 def test_download_to_not_exist_dir():
     not_exist_dir = 'not_exist_dir/'
     assert download(url, not_exist_dir) == 'Указанная директория не найдена.'
-
-
-def test_download_image():
-    with requests_mock.Mocker() as m:
-        m.get(img1, content=mock_img1)
-        with tempfile.TemporaryDirectory() as temp_dir:
-            path_to_image = download_image(img1, temp_dir)
-            result_image = open(path_to_image, 'rb').read()
-            assert result_image == mock_img1
