f1ec0498e9a359d4a6138df1d6cc3d0ca2721723
Sergey Kim <sergkim7@gmail.com>
2022-12-02 20:46:44 +0300

    upd loader with download links and scripts

--- page_loader/loader.py
+++ page_loader/loader.py
@@ -14,6 +14,7 @@ def download(url, dir=os.getcwd()):
     2) Все символы, кроме букв и цифр, заменяются на дефис -.
     3) В конце ставится .html.
     '''
+
     if not os.path.exists(dir):
         return 'Указанная директория не найдена.'
 
@@ -28,13 +29,12 @@ def download(url, dir=os.getcwd()):
 
 
 def get_page_with_saved_files(url, dir, page):
-
     files_folder_name = generate_name(url, ext='_files')
     files_path = make_files_path(dir, files_folder_name)
     soup = BeautifulSoup(page.text, 'html.parser')
-
     download_local_images(url, soup, files_folder_name, files_path)
-
+    download_local_links(url, soup, files_folder_name, files_path)
+    download_local_scripts(url, soup, files_folder_name, files_path)
     return soup.prettify()
 
 
@@ -45,9 +45,33 @@ def download_local_images(url, soup, files_folder_name, files_path):
         image_url = normalize_file_url(image['src'], url)
         image_domain = get_domain(image_url)
         if image_domain == page_domain:
-            image_relative_path = download_image(
+            image_relative_path = download_file(
                 image_url, files_folder_name, files_path)
             image['src'] = image_relative_path
+
+
+def download_local_links(url, soup, files_folder_name, files_path):
+    links = soup.find_all('link')
+    page_domain = get_domain(url)
+    for link in links:
+        link_url = normalize_file_url(link['href'], url)
+        link_domain = get_domain(link_url)
+        if link_domain == page_domain:
+            link_relative_path = download_file(
+                link_url, files_folder_name, files_path)
+            link['href'] = link_relative_path
+
+
+def download_local_scripts(url, soup, files_folder_name, files_path):
+    scripts = soup.find_all(scripts_with_src)
+    page_domain = get_domain(url)
+    for script in scripts:
+        script_url = normalize_file_url(script['src'], url)
+        script_domain = get_domain(script_url)
+        if script_domain == page_domain:
+            link_relative_path = download_file(
+                script_url, files_folder_name, files_path)
+            script['src'] = link_relative_path
 
 
 def make_files_path(dir, files_folder_name):
@@ -56,14 +80,26 @@ def make_files_path(dir, files_folder_name):
     return files_path
 
 
-def download_image(image_url, files_folder_name, files_path):
-    image = requests.get(image_url)
-    image_name = generate_name(image_url)
-    image_relative_path = generate_path(files_folder_name, image_name)
-    image_absolute_path = generate_path(files_path, image_name)
-    with open(image_absolute_path, 'wb') as f:
-        f.write(image.content)
-    return image_relative_path
+def download_file(file_url, files_folder_name, files_path):
+    images_ext = ('.JPEG', '.GIF', '.PNG', '.SVG')
+    file = requests.get(file_url)
+    file_name = generate_name(file_url)
+    file_relative_path = generate_path(files_folder_name, file_name)
+    file_absolute_path = generate_path(files_path, file_name)
+    
+    if file_url.upper().endswith(images_ext):
+        with open(file_absolute_path, 'wb') as f:
+            f.write(file.content)
+        return file_relative_path
+    
+    else:
+        with open(file_absolute_path, 'w') as f:
+            f.write(file.text)
+        return file_relative_path
+
+
+def scripts_with_src(tag):
+    return tag.name == 'script' and tag.has_attr('src')
 
 
 def normalize_page_url(url):
