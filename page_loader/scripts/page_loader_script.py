52c301703e04934496b28f593a5f3d67cf61118c
Sergey Kim <sergkim7@gmail.com>
2022-12-03 15:15:48 +0300

    upd scrript

--- page_loader/scripts/page_loader_script.py
+++ page_loader/scripts/page_loader_script.py
@@ -5,7 +5,7 @@ from page_loader.args_parser import get_args
 
 def main():
     args = get_args()
-    download(args.URL, args.output)
+    print(download(args.URL, args.output))
 
 
 if __name__ == '__main__':
