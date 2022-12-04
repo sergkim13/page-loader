c7ceee727f1cf0fc298bf6f2b52ef378a88a0138
Sergey Kim <sergkim7@gmail.com>
2022-12-04 18:41:37 +0300

    upd page-loader script

--- page_loader/scripts/page_loader_script.py
+++ page_loader/scripts/page_loader_script.py
@@ -2,6 +2,7 @@
 from page_loader import download
 from page_loader.args_parser import get_args
 import logging
+import sys
 
 logger = logging.getLogger()
 
@@ -11,7 +12,16 @@ def main():
     try:
         print(download(args.URL, args.output))
     except FileNotFoundError:
-        logger.error('Directory is not exist')
+        logger.error(f'Directory \'{args.output}\' is not exists. ')
+        sys.exit(72)
+    except PermissionError:
+        logger.error(
+            f"You don't have access to directory \'{args.output}\'."
+            f"Check permissions.")
+        sys.exit(72)
+    except FileExistsError:
+        logger.error('File is already exists.')
+        sys.exit(72)
 
 
 if __name__ == '__main__':
